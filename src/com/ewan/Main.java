d42bf212f1fafbca7deb7f61bc0bfa9533147f7e
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2020-09-03 19:25:02 -0700

    new 1900

--- src/com/ewan/Main.java
+++ src/com/ewan/Main.java
@@ -6,8 +6,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Guess a number");
         Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("what do you want to do today");
+            System.out.println("1 play Guess the number 1-100");
+            System.out.println("2 sales tax calculator");
+            int response = sc.nextInt();
+            switch (response) {
+                case 1:
+                    numberGuessing(sc);
+                    break;
+                case 2:
+                    salesTax(sc);
+                    break;
+                default:
+                    System.out.println("that is not a valid response");
+            }
+        }
+    }
+
+    static void numberGuessing(Scanner sc) {
+        System.out.println("Guess a number");
         Random rand = new Random();
         int answer = rand.nextInt(100) + 1;
         int g = 0;
@@ -20,6 +39,14 @@ public class Main {
             }
         }
         System.out.println("you win!!! it was " + answer + " yay!!!");
+
+    }
+    static void salesTax(Scanner sc){
+        System.out.println("how much is the cost?");
+        float cost = sc.nextFloat();
+        float tax =cost * 0.0725f;
+        System.out.println("the sales tax is "+ tax);
+        System.out.println("the total is "+ (cost+tax));
 
     }
 }
