787571f84a6f2dd94ede98b7a9d425c4fd4b9067
EwanThomson <67035332+EwanThomson@users.noreply.github.com>
2020-09-22 20:19:41 -0700

    grade calculator

--- src/com/ewan/Main.java
+++ src/com/ewan/Main.java
@@ -1,16 +1,17 @@
 package com.ewan;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("what do you want to do today");
             System.out.println("1 play Guess the number 1-100");
             System.out.println("2 sales tax calculator");
+            System.out.println("3 grade calculator");
             int response = sc.nextInt();
             switch (response) {
                 case 1:
@@ -18,6 +19,9 @@ public class Main {
                     break;
                 case 2:
                     salesTax(sc);
+                    break;
+                case 3:
+                    grades(sc);
                     break;
                 default:
                     System.out.println("that is not a valid response");
@@ -41,12 +45,31 @@ public class Main {
         System.out.println("you win!!! it was " + answer + " yay!!!");
 
     }
-    static void salesTax(Scanner sc){
+
+    static void salesTax(Scanner sc) {
         System.out.println("how much is the cost?");
         float cost = sc.nextFloat();
-        float tax =cost * 0.0725f;
+        float tax = cost * 0.0725f;
         System.out.println("the sales tax is "+ tax);
         System.out.println("the total is "+ (cost+tax));
+    }
 
+    static void grades(Scanner sc) {
+        System.out.println("How many grades?");
+        int numGrades = sc.nextInt();
+        ArrayList<Integer> grades = new ArrayList<>();
+        for (int i = 0; i < numGrades; i++) {
+            System.out.print("Enter grade " + (i+1) + ": ");
+            int grade = sc.nextInt();
+            grades.add(grade);
+        }
+        int total = 0;
+        System.out.print("You entered: ");
+        for (int grade : grades) {
+            System.out.print("" + grade + ", ");
+            total += grade;
+        }
+        System.out.println();
+        System.out.println("The average is " + ((float)total / numGrades));
     }
 }
